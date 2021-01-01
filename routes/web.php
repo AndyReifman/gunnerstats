2cbd116c842d9293f60c5df44780fdbe704b9034
AndyReifman <andrew@reifman.net>
2021-01-01 11:22:38 -0500

    Updating routes to include post method.

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/', 'MatchController@index');
 
 Route::get('/about', function() {
     return view('about');
