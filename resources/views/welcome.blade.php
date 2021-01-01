358a59b2c67e195a7d40465d5caa3908bddf6a9e
AndyReifman <andrew@reifman.net>
2021-01-01 10:55:47 -0500

    Updating the dropdown menu to be a form and resizing.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col s3 offset-s3">
-            <p>{!! Form::selectYear('year', 1957, now()->year) !!}</p>
-        </div>
-        <div class="col s3">
-            <p>{!! Form::selectMonth('month') !!}</p>
-        </div>
+        <form method="POST" action="/">
+            <div class="col s4 offset-s2">
+                <p>{!! Form::selectYear('year', 1957, now()->year) !!}</p>
+            </div>
+            <div class="col s4">
+                <p>{!! Form::selectMonth('month') !!}</p>
+            </div>
+            <div class="col s4">
+                <button class="btn waves-effect waves-light" type="submit" name="action">Submit<i
+                        class="material-icons right">send</i></button>
+        </form>
     </div>
 </div>
 @endsection
