cecaec01482780e6f62d87f447409695ef97de55
AndyReifman <andrew@reifman.net>
2021-01-01 08:40:00 -0500

    Screw it, back to materialize. Everything else is ugly.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,13 +4,13 @@
 <head>
   <title>Welcome to Gunnerstats</title>
   <!-- jQuery from Google -->
-  <!-- Compressed CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css"
-    integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-  <!-- Compressed JavaScript -->
-  <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js"
-    integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
 
   <script>
     $(document).foundation();
@@ -20,20 +20,18 @@
 
 <body>
   <nav>
-    <div class="top-bar">
-      <div class="top-bar-left">
-        <ul class="menu">
-          <a href="/" class="menu-text">Gunnerstats</a>
-          <li class="{{ Request::path() === '/' ? 'active' : ''}}"><a href="/">Home</a>
-          </li>
-          <li class="{{ Request::path() === 'about' ? 'active' : ''}}">
-            <a href="/about">About</a>
-          </li>
-          <li class="{{ Request::path() === 'contact' ? 'active' : ''}}">
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
+    <div class="nav-wrapper">
+      <a href="/" class="brand-logo center">Gunnerstats</a>
+      <ul class="left hide-on-med-and-down">
+        <li class="{{ Request::path() === '/' ? 'active' : ''}}"><a href="/">Home</a>
+        </li>
+        <li class="{{ Request::path() === 'about' ? 'active' : ''}}">
+          <a href="/about">About</a>
+        </li>
+        <li class="{{ Request::path() === 'contact' ? 'active' : ''}}">
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
     </div>
   </nav>
 
