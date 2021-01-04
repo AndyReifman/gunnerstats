731374ad94f5293b33cd9af27aae6adfb73bffc9
AndyReifman <andrew@reifman.net>
2021-01-04 05:53:06 -0500

    I think I want to display goals and cards in the show page as well.

--- resources/views/matches/show.blade.php
+++ resources/views/matches/show.blade.php
@@ -4,33 +4,45 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col s6 offset-s3"><h3>{{ $match->{'home/away'} == "Home" ? 'Arsenal' : $match->opposition }} - {{ $match->{'home/away'} == "Home" ? $match->opposition : 'Arsenal' }}</h3></div>
-            <div class="col s3 offset-s5"> Venue: {{ $match->venue }} </div>
+        <div class="col s6 offset-s3">
+            <h3>{{ $match->{'home/away'} == "Home" ? 'Arsenal' : $match->opposition }} -
+                {{ $match->{'home/away'} == "Home" ? $match->opposition : 'Arsenal' }}</h3>
+        </div>
+        <div class="col s3 offset-s5"> <b>Venue:</b> {{ $match->venue }} </div>
         <div class="row">
-        <table class="col s6">
-            <thead>
-            <tr>
-            <th>{{ $match->{'home/away'} == "Home" ? 'Arsenal' : $match->opposition }}</th>
-            </tr>
-            </thead>
-            <tbody>
-                @foreach ($match->homeTeam as $player)
-                <tr><td> {{ $player->playerName }} </td></tr>
-                @endforeach
-            </tbody>
-        </table>
-        <table class="col s6">
-            <thead>
-            <tr>
-            <th style="text-align: right;">{{ $match->{'home/away'} == "Home" ? $match->opposition : 'Arsenal' }}</th>
-            </tr>
-            </thead>
-            <tbody>
-                @foreach ($match->homeTeam as $player)
-                <tr><td style="text-align: right;"> {{ $player->playerName }} </td></tr>
-                @endforeach
-            </tbody>
-        </table>
+            <table class="col s6">
+                <thead>
+                    <tr>
+                        <th>{{ $match->{'home/away'} == "Home" ? 'Arsenal' : $match->opposition }}</th>
+                        <th>Goal</th>
+                        <th>Card</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($match->homeTeam as $player)
+                    <tr>
+                        <td> {{ $player->playerName }} </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <table class="col s6">
+                <thead>
+                    <tr>
+                        <th>Goal</th>
+                        <th>Card</th>
+                        <th style="text-align: right;">
+                            {{ $match->{'home/away'} == "Home" ? $match->opposition : 'Arsenal' }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($match->homeTeam as $player)
+                    <tr>
+                        <td style="text-align: right;"> {{ $player->playerName }} </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
